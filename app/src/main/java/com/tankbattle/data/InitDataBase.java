a24d11010121f0cba976049120a00eb80d76a1e0
ochauvie <olivier.chauvie.prestataire@sfr.com>
2015-09-08 10:12:11 +0200

    Init database

--- app/src/main/java/com/tankbattle/data/InitDataBase.java
+++ app/src/main/java/com/tankbattle/data/InitDataBase.java
@@ -15,28 +15,37 @@ public class InitDataBase {
         TankService.deleteAllTanks();
 
         // Russe
-        Tank tank = new Tank("KV-1", 10, Nation.RU, Genre.HEAVY);
-        tank.save();
-
-        tank = new Tank("T34", 10, Nation.RU, Genre.MEDIUM);
-        tank.save();
-
-        tank = new Tank("ISU 152", 10, Nation.RU, Genre.DESTROYER);
-        tank.save();
+        Tank tank = new Tank("KV-1", 10, Nation.RU, Genre.HEAVY); tank.save();
+        tank = new Tank("KV-2", 10, Nation.RU, Genre.HEAVY); tank.save();
+        tank = new Tank("T-55", 10, Nation.RU, Genre.HEAVY); tank.save();
+        tank = new Tank("T-34/76", 10, Nation.RU, Genre.MEDIUM); tank.save();
+        tank = new Tank("ISu 152", 10, Nation.RU, Genre.DESTROYER); tank.save();
+        tank = new Tank("Su-85", 10, Nation.RU, Genre.DESTROYER); tank.save();
 
         // Allemand
-        tank = new Tank("Tigre 1", 10, Nation.DE, Genre.HEAVY);
-        tank.save();
+        tank = new Tank("Tigre 1", 10, Nation.DE, Genre.HEAVY); tank.save();
+        tank = new Tank("Tigre 1 (Fin de production)", 10, Nation.DE, Genre.HEAVY); tank.save();
+        tank = new Tank("Ferdinand", 10, Nation.DE, Genre.HEAVY); tank.save();
+        tank = new Tank("Jagdpanther", 10, Nation.DE, Genre.DESTROYER); tank.save();
+        tank = new Tank("Jagdtiger (Henschel)", 10, Nation.DE, Genre.DESTROYER); tank.save();
+        tank = new Tank("Sturmgeschutz III Ausf.E Sturmgeschu", 10, Nation.DE, Genre.DESTROYER); tank.save();
+        tank = new Tank("Sdkfz 138 Marder III Ausf M", 10, Nation.DE, Genre.DESTROYER); tank.save();
+        tank = new Tank("10.5cm LE FH18 Auf39-H", 10, Nation.DE, Genre.ARTILLERY); tank.save();
 
         // Anglais
-        tank = new Tank("Churchill", 10, Nation.EN, Genre.HEAVY);
-        tank.save();
+        tank = new Tank("Churchill MK.III", 10, Nation.EN, Genre.HEAVY); tank.save();
+        tank = new Tank("AS-90 SPG", 10, Nation.EN, Genre.ARTILLERY); tank.save();
 
         // Americain
-        tank = new Tank("Sherman", 10, Nation.US, Genre.HEAVY);
-        tank.save();
+        tank = new Tank("M1A1 - IFOR", 10, Nation.US, Genre.HEAVY); tank.save();
+        tank = new Tank("M4A3 Sherman", 10, Nation.US, Genre.MEDIUM); tank.save();
+        tank = new Tank("M4A1 Sherman", 10, Nation.US, Genre.MEDIUM); tank.save();
+
+        // Francais
+        tank = new Tank("B1 bis", 10, Nation.FR, Genre.HEAVY); tank.save();
 
 
-
+//
+//                  Cromwell + Auto + Moderne
     }
 }
