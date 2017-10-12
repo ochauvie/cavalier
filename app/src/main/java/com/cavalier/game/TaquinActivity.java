8c7dcdbe6d2760be2830e7f509fbfeacc67b3756
ochauvie <olivier.chauvie.prestataire@sfr.com>
2017-10-12 15:06:38 +0200

    Taquin

--- app/src/main/java/com/cavalier/game/TaquinActivity.java
+++ app/src/main/java/com/cavalier/game/TaquinActivity.java
@@ -1,6 +1,7 @@
 package com.cavalier.game;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cavalier.R;
@@ -15,12 +17,18 @@ import com.cavalier.model.Monture;
 import com.cavalier.service.MontureService;
 import com.cavalier.tools.PictureUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class TaquinActivity extends Activity {
 
     private GridLayout taquin;
+    private List<ImageView> views = new ArrayList<>();
+    private TextView textView;
+    private int iter;
+    private Random r = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +37,10 @@ public class TaquinActivity extends Activity {
 
         taquin = (GridLayout) findViewById(R.id.gridLayout);
 
-
         int screenPixel=this.getWindowManager().getDefaultDisplay().getWidth();
-
         int imgWidth = 256;
         int imgHeight = 280;
         int columns = (int) ((float) screenPixel / (float) imgWidth);
-
         taquin.setColumnCount(columns);
 
         List<Monture> montures = MontureService.getAll();
@@ -50,21 +55,37 @@ public class TaquinActivity extends Activity {
                     imgView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                     imgView.setContentDescription(monture.getNom());
 
-
                     imgView.setOnClickListener(new View.OnClickListener() {
-
-                        @Override
+                    @Override
                         public void onClick(View v) {
-                            Toast.makeText(getBaseContext(), v.getContentDescription(), Toast.LENGTH_LONG).show();
+
+                            if (textView.getText().toString().equals(v.getContentDescription())) {
+                                Toast toast = Toast.makeText(getBaseContext(), "Bravo !", Toast.LENGTH_LONG);
+                                TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                                tv.setTextColor(Color.GREEN);
+                                toast.show();
+                                iter = r.nextInt(views.size());
+                                textView.setText(views.get(iter).getContentDescription());
+                            } else {
+                                Toast toast = Toast.makeText(getBaseContext(), "Non", Toast.LENGTH_SHORT);
+                                TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                                tv.setTextColor(Color.RED);
+                                toast.show();
+                            }
                         }
-
                     });
-
+                    views.add(imgView);
                     taquin.addView(imgView);
                 }
             }
         }
 
+        textView = (TextView) findViewById(R.id.textView);
+
+        iter = r.nextInt(views.size());
+        if (views.size() > 0) {
+            textView.setText(views.get(iter).getContentDescription());
+        }
     }
 
     @Override
